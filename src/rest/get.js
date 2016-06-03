c308a4f5d7417250dc35a23c7555928dca3cf926
He Zhang <hezhan@microsoft.com>
2016-06-03 12:31:27 +0800

    support query like projects(000)

--- src/rest/get.js
+++ src/rest/get.js
@@ -1,13 +1,12 @@
 export default (req, MongooseModel) => new Promise((resolve, reject) => {
-  MongooseModel.findById(req.params.id, (err, entity) => {
-    if (err) {
+  MongooseModel.find({id: req.params.id})
+    .then(entity => {
+      if (!entity) {
+        return reject({status: 404}, {text: 'Not found.'});
+      }
+      return resolve({ entity });
+    })
+    .catch(err => {
       return reject(err);
-    }
-
-    if (!entity) {
-      return reject({ status: 404 }, { text: 'Not Found' });
-    }
-
-    return resolve({ entity });
-  });
+    });
 });
